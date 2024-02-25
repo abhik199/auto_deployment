40b7ce74d3c314ef9e8e3892d1708b63865f33bc
abhik199 <abhishekkirar2004@gmail.com>
2024-02-26 00:52:10 +0530

    change workflow

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 4100;
 
 app.get("/", function (req, res) {
-  return res.json({ message: "this is testing server" });
+  return res.json({ message: "my personal server" });
 });
 
 app.get("/api", function (req, res) {
