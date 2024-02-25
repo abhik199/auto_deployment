ef1fdf3734fe634fe90b5b57bf0d22248b5ed9db
abhik199 <abhishekkirar2004@gmail.com>
2024-02-26 01:39:41 +0530

    chnage layout

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 4100;
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  return res.json({ message: "my personal server new server", port: port });
+  return res.render("index");
 });
 
-app.get("/api", function (req, res) {
-  return res.json({
-    message:
-      "To use nested populate in Mongoose, you can chain multiple calls to the populate() method in a query chain. For example, let's say you have the following schema:",
-  });
-});
-
-app.get("/hello", function (req, res) {
-  res.json({ message: "new hello world" });
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(port, function () {
   console.log(`listening on ${port}`);
