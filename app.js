e90290b763b2602508070855477d3c60cbea1673
abhik199 <abhishekkirar2004@gmail.com>
2024-02-26 01:21:00 +0530

    solve

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 4100;
 
 app.get("/", function (req, res) {
-  return res.json({ message: "my personal server" });
+  return res.json({ message: "my personal server new server", port: port });
 });
 
 app.get("/api", function (req, res) {
