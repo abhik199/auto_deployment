8e163504df0013e566bcc8394fba8a5cefce34bf
abhik199 <abhishekkirar2004@gmail.com>
2024-02-26 00:33:00 +0530

    new routes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   return res.json({ message: "this is testing server" });
 });
 
+app.get("/api", function (req, res) {
+  return res.json({ message: "abhishek kirar" });
+});
+
 app.listen(port, function () {
   console.log(`listening on ${port}`);
 });
